0ece0e136c75c3dac448790f82dee97f23727bea
shahzu01 <ulfat.shahzad@gmail.com>
2026-08-22 19:35:11 -0400

    updated index.html and Service worker

--- service-worker.js
+++ service-worker.js
@@ -27,6 +27,10 @@ self.addEventListener("activate", (event) => {
 });
 
 self.addEventListener("fetch", (event) => {
+  const url = new URL(event.request.url);
+  if (url.origin !== self.location.origin) {
+    return; // let Firestore / Google API traffic pass through untouched
+  }
   event.respondWith(
     fetch(event.request)
       .then((response) => {
